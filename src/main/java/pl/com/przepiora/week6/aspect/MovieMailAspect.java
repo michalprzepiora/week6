4a2a781065e2240338e61fb5a9fbac489d9549d2
michalprzepioraCTP <michal.przepiora@gmail.com>
2019-10-28 13:44:30 +0100

    Mail service done

--- src/main/java/pl/com/przepiora/week6/aspect/MovieMailAspect.java
+++ src/main/java/pl/com/przepiora/week6/aspect/MovieMailAspect.java
@@ -4,34 +4,27 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pl.com.przepiora.week6.service.MailService;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Aspect
 @Component
 public class MovieMailAspect {
+    private final MailService mailService;
+    private final static String MAIL = "michal.przepiora@gmail.com";
+    private final static String SUBJECT = "Information RestApi Movie";
+    private final static String MESSAGE = "New movie was added to repository";
 
-    private final Logger log = LoggerFactory.getLogger(MovieMailAspect.class);
-
-    private final JavaMailSender mailSender;
-
-    public MovieMailAspect(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
+    public MovieMailAspect(MailService mailService) {
+        this.mailService = mailService;
     }
 
     @Before("execution(void pl.com.przepiora.week6.controller.MovieApi.addMovie(*))")
     public void test() {
-        SimpleMailMessage message = new SimpleMailMessage();
-        String date = LocalDateTime.now().toString();
-        log.info("Mail is sending...");
-        message.setTo("michal.przepiora@gmail.com");
-        message.setSubject("Information");
-        message.setText("New movie was added to repository. \n" + date);
-        mailSender.send(message);
-        log.info("Mail was send.");
+        mailService.send(MAIL, SUBJECT, MESSAGE + "\n" + LocalDateTime.now());
     }
-
 }
